6b3ec4a93fa5abfb5afcb60b3f329475e0ceaf59
Edson Li <edsonli1999@gmail.com>
2024-02-15 16:22:49 +1100

    Added in Weather object class that makes index.html cleaner, accessing these attributes within this class instead of straight up in the html

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
   def index
     @message = "Edson's Weather App"
 
-    # Pulling data from openWeather using API key and location of melbourne
-    @data = CurrentWeatherService.new(latitude: "-37.8136" , longitude: "144.9631" , units: "metric").call
+    # https://api.openweathermap.org/data/2.5/weather?lat=-37.8136&lon=144.9631&appid=ab0c803febd21bec092009126752f607&units=metric
 
+    # Pulling data from openWeather using API key and location of melbourne
+    data = CurrentWeatherService.new(latitude: "-37.8136" , longitude: "144.9631" , units: "metric").call
+    @weather = Weather.new(data)
   end
 end
---
+++ app/models/weather.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# Presentor pattern: taking the json data and wrapping it into a ruby object, making it more intuitive in
+# views/home/index.html.erb page. When someone who hasn't worked on this project before now looks at that
+# html page, they won't need to insert a raise to find out what @data contains
+class Weather
+  def initialize(data)
+    @data = data
+  end
+
+  def icon_url
+    # current_weather['icon'] === @data["weather"].first["icon"]
+    "http://openweathermap.org/img/wn/#{current_weather['icon']}@2x.png"
+  end
+
+  def description
+    current_weather["description"]
+  end
+
+  def location
+    data['name']
+  end
+
+  def temperature
+    # data['main']['temp']
+    data.dig('main', 'temp')
+  end
+
+  def feels_like
+    # data['main']['feels_like']
+    data.dig('main', 'feels_like')
+  end
+
+  private
+
+  attr_reader :data
+
+  def current_weather
+    @current_weather ||= data["weather"].first
+  end
+end
